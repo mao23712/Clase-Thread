9234addde639e90770023ab3b9581a2aa2ec7d4c
mao_programmer <100383163+mao23712@users.noreply.github.com>
2023-04-14 16:56:49 -0400

    Agregar archivos de proyecto.

---
+++ Clase Thread/Hilo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+public class Hilo
+{
+    private string mensaje;
+    private Thread hilo;
+
+    public Hilo(string mensaje)
+    {
+        this.mensaje = mensaje;
+        hilo = new Thread(new ThreadStart(this.Run));
+    }
+
+    public void Start()
+    {
+        hilo.Start();
+    }
+
+    public void Join()
+    {
+        hilo.Join();
+    }
+
+    public void Run()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(mensaje);
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Clase Thread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hilo hilo1 = new Hilo("Soy el hilo 1");
+        Hilo hilo2 = new Hilo("Soy el hilo 2");
+        Hilo hilo3 = new Hilo("Soy el hilo 3");
+
+        hilo1.Start();
+        hilo2.Start();
+        hilo3.Start();
+
+        hilo1.Join();
+        hilo2.Join();
+        hilo3.Join();
+
+        Console.WriteLine("Todos los hilos han terminado");
+        Console.ReadKey();
+    }
+}
+
+
+
+
+
